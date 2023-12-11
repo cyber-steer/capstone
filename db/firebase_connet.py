418bf284bff7250bce9e57d4c54f742c7e0de778
cyber-steer <cybersteer@naver.com>
2023-12-11 18:40:25 +0900

    reset

--- db/firebase_connet.py
+++
@@ -1,9 +0,0 @@
-import json
-import pyrebase
-
-
-class Firebase:
-    def __init__(self, path):
-        with open(path) as f:
-            config = json.load(f)
-        self.firebase = pyrebase.initialize_app(config)
